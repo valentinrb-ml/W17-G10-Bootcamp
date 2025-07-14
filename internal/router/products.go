425c5a541dab3aee843742096b083b8fb15bc418
MateoRr88 <mateo.ramirezrubio@mercadolibre.com.co>
2025-07-14 02:09:16 -0500

    feat: implement organized product routing with middleware integration

--- internal/router/products.go
+++ internal/router/products.go
@@ -3,14 +3,18 @@ package router
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/varobledo_meli/W17-G10-Bootcamp.git/internal/handler"
+	"github.com/varobledo_meli/W17-G10-Bootcamp.git/internal/middleware"
 )
 
 func MountProductRoutes(api chi.Router, hd *handler.ProductHandler) {
 	api.Route("/products", func(r chi.Router) {
-		r.Get("/", hd.GetAll)
-		r.Get("/{id}", hd.GetByID)
-		r.Post("/", hd.Create)
-		r.Patch("/{id}", hd.Patch)
-		r.Delete("/{id}", hd.Delete)
+		r.With(middleware.OnlyGET).Get("/", hd.GetAll)
+		r.With(middleware.OnlyPOST).Post("/", hd.Create)
+
+		r.Route("/{id}", func(r chi.Router) {
+			r.With(middleware.OnlyGET).Get("/", hd.GetByID)
+			r.With(middleware.OnlyPATCH).Patch("/", hd.Patch)
+			r.With(middleware.OnlyDELETE).Delete("/", hd.Delete)
+		})
 	})
 }
