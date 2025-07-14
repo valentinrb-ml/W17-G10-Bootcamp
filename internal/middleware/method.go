cddac97e8a7b60a210eef3dd93d1b1b109297563
MateoRr88 <mateo.ramirezrubio@mercadolibre.com.co>
2025-07-14 02:07:51 -0500

    feat: add HTTP method validation middleware

--- internal/middleware/method.go
+++ internal/middleware/method.go
@@ -1,1 +1,32 @@
 package middleware
+
+import (
+	"github.com/varobledo_meli/W17-G10-Bootcamp.git/pkg/api/apperrors"
+	"github.com/varobledo_meli/W17-G10-Bootcamp.git/pkg/api/response"
+	"net/http"
+)
+
+func AllowMethods(methods ...string) func(http.Handler) http.Handler {
+	methodMap := make(map[string]bool, len(methods))
+	for _, method := range methods {
+		methodMap[method] = true
+	}
+
+	return func(next http.Handler) http.Handler {
+		return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+			if !methodMap[r.Method] {
+				response.Error(w, apperrors.NewAppError(apperrors.CodeMethodNotAllowed, "method not allowed"))
+				return
+			}
+			next.ServeHTTP(w, r)
+		})
+	}
+}
+
+// Helpers for common cases
+var (
+	OnlyGET    = AllowMethods("GET")
+	OnlyPOST   = AllowMethods("POST")
+	OnlyPATCH  = AllowMethods("PATCH")
+	OnlyDELETE = AllowMethods("DELETE")
+)
