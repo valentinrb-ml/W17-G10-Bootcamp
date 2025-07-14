cb9329a798e09d75cf1371088ee28e6667086f2b
JuanCadavid18 <juan.cadavidarango@mercadolibre.com.co>
2025-07-14 14:14:22 -0500

    refactor: update models, mappers and validators to match new DB schema

--- internal/validators/section.go
+++ internal/validators/section.go
@@ -5,11 +5,11 @@ import (
 	"github.com/varobledo_meli/W17-G10-Bootcamp.git/pkg/models/section"
 )
 
-func ValidateSectionRequest(secReq section.RequestSection) *api.ServiceError {
-	if secReq.SectionNumber == nil || secReq.WarehouseId == nil ||
-		secReq.MaximumCapacity == nil || secReq.MinimumCapacity == nil ||
-		secReq.CurrentCapacity == nil || secReq.MinimumTemperature == nil ||
-		secReq.CurrentTemperature == nil || secReq.ProductId == 0 {
+func ValidateSectionRequest(secReq section.PostSection) *api.ServiceError {
+	if secReq.SectionNumber == 0 || secReq.WarehouseId == 0 ||
+		secReq.MaximumCapacity == 0 || secReq.MinimumCapacity == 0 ||
+		secReq.CurrentCapacity == 0 || secReq.MinimumTemperature == nil ||
+		secReq.CurrentTemperature == nil || secReq.ProductTypeId == 0 {
 		orig := api.ServiceErrors[api.ErrUnprocessableEntity]
 		err := api.ServiceError{
 			Code:         orig.Code,
@@ -18,14 +18,39 @@ func ValidateSectionRequest(secReq section.RequestSection) *api.ServiceError {
 		}
 		return &err
 	}
+	if secReq.MaximumCapacity < 0 || secReq.MinimumCapacity < 0 || secReq.CurrentCapacity < 0 {
+		orig := api.ServiceErrors[api.ErrUnprocessableEntity]
+		return &api.ServiceError{
+			Code:         orig.Code,
+			ResponseCode: orig.ResponseCode,
+			Message:      "Capacity values cannot be negative.",
+		}
+	}
+	if secReq.MaximumCapacity < secReq.MinimumCapacity {
+		errDef := api.ServiceErrors[api.ErrUnprocessableEntity]
+		return &api.ServiceError{
+			Code:         errDef.Code,
+			ResponseCode: errDef.ResponseCode,
+			Message:      "Maximum capacity cannot be less than minimum capacity.",
+		}
+	}
+	
+	if secReq.CurrentCapacity > secReq.MaximumCapacity {
+		errDef := api.ServiceErrors[api.ErrUnprocessableEntity]
+		return &api.ServiceError{
+			Code:         errDef.Code,
+			ResponseCode: errDef.ResponseCode,
+			Message:      "Current capacity cannot exceed maximum capacity.",
+		}
+	}
 	return nil
 }
 
-func ValidateSectionPatch(secReq section.RequestSection) *api.ServiceError {
+func ValidateSectionPatch(secReq section.PatchSection) *api.ServiceError {
 	if secReq.SectionNumber == nil && secReq.WarehouseId == nil &&
 		secReq.MaximumCapacity == nil && secReq.MinimumCapacity == nil &&
 		secReq.CurrentCapacity == nil && secReq.MinimumTemperature == nil &&
-		secReq.CurrentTemperature == nil && secReq.ProductId == 0 {
+		secReq.CurrentTemperature == nil && secReq.ProductTypeId == nil {
 		orig := api.ServiceErrors[api.ErrUnprocessableEntity]
 		err := api.ServiceError{
 			Code:         orig.Code,
@@ -34,6 +59,52 @@ func ValidateSectionPatch(secReq section.RequestSection) *api.ServiceError {
 		}
 		return &err
 	}
+
+	if secReq.MaximumCapacity != nil && *secReq.MaximumCapacity < 0 {
+		errDef := api.ServiceErrors[api.ErrUnprocessableEntity]
+		return &api.ServiceError{
+			Code:         errDef.Code,
+			ResponseCode: errDef.ResponseCode,
+			Message:      "Maximum capacity cannot be negative.",
+		}
+	}
+	if secReq.MinimumCapacity != nil && *secReq.MinimumCapacity < 0 {
+		errDef := api.ServiceErrors[api.ErrUnprocessableEntity]
+		return &api.ServiceError{
+			Code:         errDef.Code,
+			ResponseCode: errDef.ResponseCode,
+			Message:      "Minimum capacity cannot be negative.",
+		}
+	}
+	if secReq.CurrentCapacity != nil && *secReq.CurrentCapacity < 0 {
+		errDef := api.ServiceErrors[api.ErrUnprocessableEntity]
+		return &api.ServiceError{
+			Code:         errDef.Code,
+			ResponseCode: errDef.ResponseCode,
+			Message:      "Current capacity cannot be negative.",
+		}
+	}
+
+	if secReq.MaximumCapacity != nil && secReq.MinimumCapacity != nil &&
+		*secReq.MaximumCapacity < *secReq.MinimumCapacity {
+		errDef := api.ServiceErrors[api.ErrUnprocessableEntity]
+		return &api.ServiceError{
+			Code:         errDef.Code,
+			ResponseCode: errDef.ResponseCode,
+			Message:      "Maximum capacity cannot be less than minimum capacity.",
+		}
+	}
+
+	if secReq.CurrentCapacity != nil && secReq.MaximumCapacity != nil &&
+		*secReq.CurrentCapacity > *secReq.MaximumCapacity {
+		errDef := api.ServiceErrors[api.ErrUnprocessableEntity]
+		return &api.ServiceError{
+			Code:         errDef.Code,
+			ResponseCode: errDef.ResponseCode,
+			Message:      "Current capacity cannot exceed maximum capacity.",
+		}
+	}
+
 	return nil
 
 }
--- pkg/models/section/section.go
+++ pkg/models/section/section.go
@@ -1,36 +1,46 @@
 package section
 
-type RequestSection struct {
-	SectionNumber      *int `json:"section_number"`
-	CurrentTemperature *int `json:"current_temperature"`
-	MinimumTemperature *int `json:"minimum_temperature"`
-	CurrentCapacity    *int `json:"current_capacity"`
-	MinimumCapacity    *int `json:"minimum_capacity"`
-	MaximumCapacity    *int `json:"maximum_capacity"`
-	WarehouseId        *int `json:"warehouse_id"`
-	ProductId          int  `json:"product_id"`
+type PostSection struct {
+	SectionNumber      int      `json:"section_number"`
+	CurrentTemperature *float64 `json:"current_temperature"`
+	MinimumTemperature *float64 `json:"minimum_temperature"`
+	CurrentCapacity    int      `json:"current_capacity"`
+	MinimumCapacity    int      `json:"minimum_capacity"`
+	MaximumCapacity    int      `json:"maximum_capacity"`
+	WarehouseId        int      `json:"warehouse_id"`
+	ProductTypeId      int      `json:"product_type_id"`
+}
+type PatchSection struct {
+	SectionNumber      *int     `json:"section_number"`
+	CurrentTemperature *float64 `json:"current_temperature"`
+	MinimumTemperature *float64 `json:"minimum_temperature"`
+	CurrentCapacity    *int     `json:"current_capacity"`
+	MinimumCapacity    *int     `json:"minimum_capacity"`
+	MaximumCapacity    *int     `json:"maximum_capacity"`
+	WarehouseId        *int     `json:"warehouse_id"`
+	ProductTypeId      *int     `json:"product_type_id"`
 }
 
 type ResponseSection struct {
-	Id                 int `json:"id"`
-	SectionNumber      int `json:"section_number"`
-	CurrentTemperature int `json:"current_temperature"`
-	MinimumTemperature int `json:"minimum_temperature"`
-	CurrentCapacity    int `json:"current_capacity"`
-	MinimumCapacity    int `json:"minimum_capacity"`
-	MaximumCapacity    int `json:"maximum_capacity"`
-	WarehouseId        int `json:"warehouse_id"`
-	ProductId          int `json:"product_id"`
+	Id                 int     `json:"id"`
+	SectionNumber      int     `json:"section_number"`
+	CurrentTemperature float64 `json:"current_temperature"`
+	MinimumTemperature float64 `json:"minimum_temperature"`
+	CurrentCapacity    int     `json:"current_capacity"`
+	MinimumCapacity    int     `json:"minimum_capacity"`
+	MaximumCapacity    int     `json:"maximum_capacity"`
+	WarehouseId        int     `json:"warehouse_id"`
+	ProductTypeId      int     `json:"product_type_id"`
 }
 
 type Section struct {
-	Id                 int `json:"id"`
-	SectionNumber      int `json:"section_number"`
-	CurrentTemperature int `json:"current_temperature"`
-	MinimumTemperature int `json:"minimum_temperature"`
-	CurrentCapacity    int `json:"current_capacity"`
-	MinimumCapacity    int `json:"minimum_capacity"`
-	MaximumCapacity    int `json:"maximum_capacity"`
-	WarehouseId        int `json:"warehouse_id"`
-	ProductId          int `json:"product_id"`
+	Id                 int     `json:"id"`
+	SectionNumber      int     `json:"section_number"`
+	CurrentTemperature float64 `json:"current_temperature"`
+	MinimumTemperature float64 `json:"minimum_temperature"`
+	CurrentCapacity    int     `json:"current_capacity"`
+	MinimumCapacity    int     `json:"minimum_capacity"`
+	MaximumCapacity    int     `json:"maximum_capacity"`
+	WarehouseId        int     `json:"warehouse_id"`
+	ProductTypeId      int     `json:"product_type_id"`
 }
