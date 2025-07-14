60c7b42e05636c57e47a8916144b6f93a49d6704
maxfarias_meli <maximiliano.farias@mercadolibre.com>
2025-07-14 10:02:10 -0300

    fix: The service and repository interfaces are separated into a file

--- internal/repository/warehouse.go
+++ internal/repository/warehouse.go
@@ -9,14 +9,7 @@ import (
 	"github.com/varobledo_meli/W17-G10-Bootcamp.git/pkg/models/warehouse"
 )
 
-type WarehouseRepository interface {
-	Create(ctx context.Context, w warehouse.Warehouse) (*warehouse.Warehouse, *api.ServiceError)
-	Exist(ctx context.Context, wc string) (bool, *api.ServiceError)
-	FindAll(ctx context.Context) ([]warehouse.Warehouse, *api.ServiceError)
-	FindById(ctx context.Context, id int) (*warehouse.Warehouse, *api.ServiceError)
-	Update(ctx context.Context, id int, w warehouse.Warehouse) (*warehouse.Warehouse, *api.ServiceError)
-	Delete(ctx context.Context, id int) *api.ServiceError
-}
+
 
 const (
 	queryWarehouseExist    = `SELECT COUNT(*) FROM warehouse WHERE warehouse_code = ?`
@@ -26,14 +19,6 @@ const (
 	queryWarehouseUpdate   = `UPDATE warehouse SET warehouse_code = ?, address = ?, minimum_temperature = ?, minimum_capacity = ?, telephone = ? WHERE id = ?`
 	queryWarehouseDelete   = `DELETE FROM warehouse WHERE id = ?`
 )
-
-type WarehouseMySQL struct {
-	db *sql.DB
-}
-
-func NewWarehouseRepository(db *sql.DB) *WarehouseMySQL {
-	return &WarehouseMySQL{db}
-}
 
 func (r *WarehouseMySQL) Exist(ctx context.Context, wc string) (bool, *api.ServiceError) {
 	var count int
--- internal/service/warehouse.go
+++ internal/service/warehouse.go
@@ -5,27 +5,10 @@ import (
 	"sort"
 
 	"github.com/varobledo_meli/W17-G10-Bootcamp.git/internal/mappers"
-	"github.com/varobledo_meli/W17-G10-Bootcamp.git/internal/repository"
 	"github.com/varobledo_meli/W17-G10-Bootcamp.git/internal/validators"
 	"github.com/varobledo_meli/W17-G10-Bootcamp.git/pkg/api"
 	"github.com/varobledo_meli/W17-G10-Bootcamp.git/pkg/models/warehouse"
 )
-
-type WarehouseService interface {
-	Create(ctx context.Context, w warehouse.Warehouse) (*warehouse.Warehouse, *api.ServiceError)
-	FindAll(ctx context.Context) ([]warehouse.Warehouse, *api.ServiceError)
-	FindById(ctx context.Context, id int) (*warehouse.Warehouse, *api.ServiceError)
-	Update(ctx context.Context, id int, patch warehouse.WarehousePatchDTO) (*warehouse.Warehouse, *api.ServiceError)
-	Delete(ctx context.Context, id int) *api.ServiceError
-}
-
-type WarehouseDefault struct {
-	rp repository.WarehouseRepository
-}
-
-func NewWarehouseDefault(rp repository.WarehouseRepository) *WarehouseDefault {
-	return &WarehouseDefault{rp: rp}
-}
 
 func (s *WarehouseDefault) Create(ctx context.Context, w warehouse.Warehouse) (*warehouse.Warehouse, *api.ServiceError) {
 	ok, err := s.rp.Exist(ctx, w.WarehouseCode)
