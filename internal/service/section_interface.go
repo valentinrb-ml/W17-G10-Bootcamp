bf0ecf528d5752a9c1c5c5e5801bac7b5196d305
JuanCadavid18 <juan.cadavidarango@mercadolibre.com.co>
2025-07-14 14:23:41 -0500

    refactor: update service layer to use repositories for all operations

--- internal/service/section_interface.go
+++ internal/service/section_interface.go
@@ -1,14 +1,14 @@
 package service
 
 import (
-	"github.com/varobledo_meli/W17-G10-Bootcamp.git/pkg/api"
+	"context"
 	"github.com/varobledo_meli/W17-G10-Bootcamp.git/pkg/models/section"
 )
 
 type SectionService interface {
-	FindAllSections() ([]section.Section, *api.ServiceError)
-	FindById(id int) (section.Section, *api.ServiceError)
-	DeleteSection(id int) *api.ServiceError
-	CreateSection(sec section.Section) (section.Section, *api.ServiceError)
-	UpdateSection(id int, sec section.RequestSection) (section.Section, *api.ServiceError)
+	FindAllSections(ctx context.Context) ([]section.Section, error)
+	FindById(ctx context.Context, id int) (*section.Section, error)
+	DeleteSection(ctx context.Context, id int) error
+	CreateSection(ctx context.Context, sec section.Section) (*section.Section, error)
+	UpdateSection(ctx context.Context, id int, sec section.PatchSection) (*section.Section, error)
 }
