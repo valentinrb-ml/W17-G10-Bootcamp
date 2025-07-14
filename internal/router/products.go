d2f89c692b105eb68cb96de0cfa260bb211fa6f0
MateoRr88 <mateo.ramirezrubio@mercadolibre.com.co>
2025-07-14 10:41:29 -0500

    refactor: integrate httputil handlers across all routes for consistent JSON error responses

--- internal/router/products.go
+++ internal/router/products.go
@@ -3,18 +3,17 @@ package router
 import (
 	"github.com/go-chi/chi/v5"
 	"github.com/varobledo_meli/W17-G10-Bootcamp.git/internal/handler"
-	"github.com/varobledo_meli/W17-G10-Bootcamp.git/internal/middleware"
 )
 
 func MountProductRoutes(api chi.Router, hd *handler.ProductHandler) {
 	api.Route("/products", func(r chi.Router) {
-		r.With(middleware.OnlyGET).Get("/", hd.GetAll)
-		r.With(middleware.OnlyPOST).Post("/", hd.Create)
+		r.Get("/", hd.GetAll)
+		r.Post("/", hd.Create)
 
 		r.Route("/{id}", func(r chi.Router) {
-			r.With(middleware.OnlyGET).Get("/", hd.GetByID)
-			r.With(middleware.OnlyPATCH).Patch("/", hd.Patch)
-			r.With(middleware.OnlyDELETE).Delete("/", hd.Delete)
+			r.Get("/", hd.GetByID)
+			r.Patch("/", hd.Patch)
+			r.Delete("/", hd.Delete)
 		})
 	})
 }
