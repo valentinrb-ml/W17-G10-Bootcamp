738a6420f3d91dc5d9c2b991f2caea0ac0c307c0
nikole-meli <karen.moralesrosas@mercadolibre.com.mx>
2025-07-16 00:20:57 -0600

    fix: date format

--- internal/mappers/purchase_order.go
+++ internal/mappers/purchase_order.go
@@ -6,19 +6,8 @@ import (
 	models "github.com/varobledo_meli/W17-G10-Bootcamp.git/pkg/models/buyer"
 )
 
-/*
-*
-
-	func RequestPurchaseOrderToPurchaseOrder(req models.RequestPurchaseOrder) (models.PurchaseOrder, error) {
-		orderDate, err := time.Parse("2006-01-02", req.OrderDate)
-		if err != nil {
-			return models.PurchaseOrder{}, err
-		}
-
-*
-*/
 func RequestPurchaseOrderToPurchaseOrder(req models.RequestPurchaseOrder) (models.PurchaseOrder, error) {
-	orderDate, err := time.Parse("2006-01-02 15:04:05", req.OrderDate)
+	orderDate, err := time.Parse("2006-01-02", req.OrderDate)
 	if err != nil {
 		return models.PurchaseOrder{}, err
 	}
