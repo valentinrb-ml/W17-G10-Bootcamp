68d851b58e1dcee6601637affb10fb1a096faa49
MateoRr88 <mateo.ramirezrubio@mercadolibre.com.co>
2025-07-16 11:04:02 -0500

    fix: temporarily restore legacy ParseIDParam to unblock build after rebase (to be removed later)

--- pkg/api/httputil/request.go
+++ pkg/api/httputil/request.go
@@ -2,9 +2,11 @@ package httputil
 
 import (
 	"encoding/json"
+	"github.com/go-chi/chi/v5"
 	"github.com/varobledo_meli/W17-G10-Bootcamp.git/pkg/api/apperrors"
 	"github.com/varobledo_meli/W17-G10-Bootcamp.git/pkg/api/response"
 	"net/http"
+	"strconv"
 )
 
 func MethodNotAllowedHandler(w http.ResponseWriter, r *http.Request) {
@@ -26,4 +28,22 @@ func DecodeJSON(r *http.Request, dst interface{}) error {
 		return apperrors.NewAppError(apperrors.CodeBadRequest, "invalid JSON format")
 	}
 	return nil
+}
+
+func ParseIDParam(r *http.Request, paramName string) (int, error) {
+	idStr := chi.URLParam(r, paramName)
+	if idStr == "" {
+		return 0, apperrors.NewAppError(apperrors.CodeBadRequest, paramName+" parameter is required")
+	}
+
+	id, err := strconv.Atoi(idStr)
+	if err != nil {
+		return 0, apperrors.NewAppError(apperrors.CodeBadRequest, paramName+" must be a valid integer")
+	}
+
+	if id <= 0 {
+		return 0, apperrors.NewAppError(apperrors.CodeBadRequest, paramName+" must be a positive integer")
+	}
+
+	return id, nil
 }
