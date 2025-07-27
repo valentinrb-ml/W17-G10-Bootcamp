3be440f62b28e4eafdf08b26ff1e3f497bf81fb4
nikole-meli <karen.moralesrosas@mercadolibre.com.mx>
2025-07-27 01:55:15 -0600

    feat: Add functions to the mock for service tests

--- mocks/buyer/buyer_repository_mock.go
+++ mocks/buyer/buyer_repository_mock.go
@@ -1,4 +1,3 @@
-// buyer_repository_mock.go
 package mocks
 
 import (
@@ -8,19 +7,21 @@ import (
 )
 
 type BuyerRepositoryMocks struct {
-	MockCreate           func(ctx context.Context, b *models.Buyer) (*models.Buyer, error)
-	MockFindAll          func(ctx context.Context) ([]*models.Buyer, error)
+	MockCreate           func(ctx context.Context, b models.Buyer) (*models.Buyer, error)
+	MockFindAll          func(ctx context.Context) ([]models.Buyer, error)
 	MockFindByID         func(ctx context.Context, id int) (*models.Buyer, error)
+	MockFindById         func(ctx context.Context, id int) (*models.Buyer, error)
 	MockFindByCardNumber func(ctx context.Context, cardNumber string) (*models.Buyer, error)
-	MockUpdate           func(ctx context.Context, id int, b *models.Buyer) error
+	MockUpdate           func(ctx context.Context, id int, b models.Buyer) error
 	MockDelete           func(ctx context.Context, id int) error
+	MockCardNumberExists func(ctx context.Context, cardNumber string, id int) bool
 }
 
-func (m *BuyerRepositoryMocks) Create(ctx context.Context, b *models.Buyer) (*models.Buyer, error) {
+func (m *BuyerRepositoryMocks) Create(ctx context.Context, b models.Buyer) (*models.Buyer, error) {
 	return m.MockCreate(ctx, b)
 }
 
-func (m *BuyerRepositoryMocks) FindAll(ctx context.Context) ([]*models.Buyer, error) {
+func (m *BuyerRepositoryMocks) FindAll(ctx context.Context) ([]models.Buyer, error) {
 	return m.MockFindAll(ctx)
 }
 
@@ -28,14 +29,22 @@ func (m *BuyerRepositoryMocks) FindByID(ctx context.Context, id int) (*models.Bu
 	return m.MockFindByID(ctx, id)
 }
 
+func (m *BuyerRepositoryMocks) FindById(ctx context.Context, id int) (*models.Buyer, error) {
+	return m.MockFindById(ctx, id)
+}
+
 func (m *BuyerRepositoryMocks) FindByCardNumber(ctx context.Context, cardNumber string) (*models.Buyer, error) {
 	return m.MockFindByCardNumber(ctx, cardNumber)
 }
 
-func (m *BuyerRepositoryMocks) Update(ctx context.Context, id int, b *models.Buyer) error {
+func (m *BuyerRepositoryMocks) Update(ctx context.Context, id int, b models.Buyer) error {
 	return m.MockUpdate(ctx, id, b)
 }
 
 func (m *BuyerRepositoryMocks) Delete(ctx context.Context, id int) error {
 	return m.MockDelete(ctx, id)
+}
+
+func (m *BuyerRepositoryMocks) CardNumberExists(ctx context.Context, cardNumber string, id int) bool {
+	return m.MockCardNumberExists(ctx, cardNumber, id)
 }
