e12618c51df19707fabf3c6ed2766d71c0e30117
JuanCadavid18 <juan.cadavidarango@mercadolibre.com.co>
2025-07-28 15:01:18 -0500

    test: add service tests for product batches and corresponding repository mock

---
+++ internal/service/product_batch/product_batch_create_test.go
@@ -0,0 +1,73 @@
+package service_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	service "github.com/varobledo_meli/W17-G10-Bootcamp.git/internal/service/product_batch"
+	mocks "github.com/varobledo_meli/W17-G10-Bootcamp.git/mocks/product_batch"
+	models "github.com/varobledo_meli/W17-G10-Bootcamp.git/pkg/models/product_batches"
+	"github.com/varobledo_meli/W17-G10-Bootcamp.git/testhelpers"
+)
+
+func TestProductBatchesService_CreateProductBatches(t *testing.T) {
+	type arrange struct {
+		repoMock func() *mocks.ProductBatchMock
+	}
+	type output struct {
+		expected      *models.ProductBatches
+		expectedError bool
+		err           error
+	}
+	type input struct {
+		batch models.ProductBatches
+	}
+	type testCase struct {
+		name string
+		arrange
+		output
+		input
+	}
+
+	prodBatch := testhelpers.DummyProductBatch(1)
+
+	testCases := []testCase{
+		{
+			name: "returns new product batch on successful creation",
+			arrange: arrange{
+				repoMock: func() *mocks.ProductBatchMock {
+					return &mocks.ProductBatchMock{
+						FuncCreate: func(ctx context.Context, proBa models.ProductBatches) (*models.ProductBatches, error) {
+							dummy := testhelpers.DummyProductBatch(1)
+							return &dummy, nil
+						},
+					}
+				},
+			},
+			input: input{batch: prodBatch},
+			output: output{
+				expected:      &prodBatch,
+				expectedError: false,
+				err:           nil,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			svc := service.NewProductBatchesService(tc.arrange.repoMock())
+
+			result, err := svc.CreateProductBatches(context.Background(), tc.input.batch)
+
+			if tc.output.expectedError {
+				require.Error(t, err)
+				require.Equal(t, tc.output.err.Error(), err.Error())
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, tc.output.expected, result)
+		})
+	}
+}
---
+++ internal/service/product_batch/product_batch_getById_test.go
@@ -0,0 +1,92 @@
+package service_test
+
+import (
+	"context"
+	"testing"
+
+	"github.com/stretchr/testify/require"
+	service "github.com/varobledo_meli/W17-G10-Bootcamp.git/internal/service/product_batch"
+	mocks "github.com/varobledo_meli/W17-G10-Bootcamp.git/mocks/product_batch"
+	models "github.com/varobledo_meli/W17-G10-Bootcamp.git/pkg/models/product_batches"
+	"github.com/varobledo_meli/W17-G10-Bootcamp.git/testhelpers"
+)
+
+func TestProductBatchesService_GetReportProductById(t *testing.T) {
+	type arrange struct {
+		repoMock func() *mocks.ProductBatchMock
+	}
+	type output struct {
+		expected      *models.ReportProduct
+		expectedError bool
+		err           error
+	}
+	type input struct {
+		sectionNumber int
+	}
+	type testCase struct {
+		name string
+		arrange
+		output
+		input
+	}
+
+	report := testhelpers.DummyReportProduct()
+
+	testCases := []testCase{
+		{
+			name: "returns report product on success",
+			arrange: arrange{
+				repoMock: func() *mocks.ProductBatchMock {
+					return &mocks.ProductBatchMock{
+						FuncGetReportById: func(ctx context.Context, sectionNumber int) (*models.ReportProduct, error) {
+							dummy := testhelpers.DummyReportProduct()
+							return &dummy, nil
+						},
+					}
+				},
+			},
+			input: input{sectionNumber: report.SectionId},
+			output: output{
+				expected:      &report,
+				expectedError: false,
+				err:           nil,
+			},
+		},
+		{
+			name: "returns error when repo fails",
+			arrange: arrange{
+				repoMock: func() *mocks.ProductBatchMock {
+					return &mocks.ProductBatchMock{
+						FuncGetReportById: func(ctx context.Context, sectionNumber int) (*models.ReportProduct, error) {
+							return nil, context.DeadlineExceeded
+						},
+					}
+				},
+			},
+			input: input{sectionNumber: 9999},
+			output: output{
+				expected:      nil,
+				expectedError: true,
+				err:           context.DeadlineExceeded,
+			},
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.name, func(t *testing.T) {
+			svc := service.NewProductBatchesService(tc.arrange.repoMock())
+
+			result, err := svc.GetReportProductById(context.Background(), tc.input.sectionNumber)
+
+			if tc.output.expectedError {
+				require.Error(t, err)
+				require.Equal(t, tc.output.err, err)
+				require.Nil(t, result)
+				return
+			}
+
+			require.NoError(t, err)
+			require.Equal(t, tc.output.expected, result)
+		})
+	}
+}
