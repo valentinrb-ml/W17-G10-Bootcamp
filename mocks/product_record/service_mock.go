2de179f8253510753fcdefd17d234e5df81317ec
MateoRr88 <mateo.ramirezrubio@mercadolibre.com.co>
2025-07-29 05:07:54 -0500

    feat(mocks): add service mock for product-record

---
+++ mocks/product_record/service_mock.go
@@ -0,0 +1,33 @@
+package product_record
+
+import (
+	"context"
+
+	"github.com/stretchr/testify/mock"
+	models "github.com/varobledo_meli/W17-G10-Bootcamp.git/pkg/models/product_record"
+)
+
+type MockProductRecordService struct {
+	mock.Mock
+}
+
+func (m *MockProductRecordService) Create(ctx context.Context, r models.ProductRecord) (models.ProductRecord, error) {
+	args := m.Called(ctx, r)
+	return args.Get(0).(models.ProductRecord), args.Error(1)
+}
+
+/*
+GetRecordsReport mimics the SELECT-report query.
+
+If the test returns nil instead of an empty slice we guard against the panic that would occur when doing nil.(slice).
+*/
+func (m *MockProductRecordService) GetRecordsReport(ctx context.Context, id int) ([]models.ProductRecordReport, error) {
+	args := m.Called(ctx, id)
+
+	// nil-safe conversion of the first return value
+	var rep []models.ProductRecordReport
+	if arg0 := args.Get(0); arg0 != nil {
+		rep = arg0.([]models.ProductRecordReport)
+	}
+	return rep, args.Error(1)
+}
